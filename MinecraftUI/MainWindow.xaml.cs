7b13232e8f4dd1ab363b1df2998f2fe85bee0a1d
Fayez Chedid <FayezJosephChedid@outlook.com>
2022-02-24 22:08:39 -0500

    Finally read output to GUI

--- MinecraftUI/MainWindow.xaml.cs
+++ MinecraftUI/MainWindow.xaml.cs
@@ -1,13 +1,15 @@
 ﻿using Microsoft.Win32;
 using System;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.IO;
-using System.Net;
+using System.Management;
+using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using System.Windows.Media.Imaging;
-using System.Management;
-using System.ComponentModel;
+using System.Windows.Threading;
 
 namespace MinecraftUI
 {
@@ -16,62 +18,63 @@ namespace MinecraftUI
     /// </summary>
     public partial class MainWindow : Window
     {
+        Process ServerProc;
+
         public MainWindow()
         {
             InitializeComponent();
 
-            ObjectQuery objectQuery = new ObjectQuery("SELECT * FROM Win32_OperatingSystem");
-            ManagementObjectSearcher managementObjectSearcher = new ManagementObjectSearcher(objectQuery);
+            ObjectQuery objectQuery = new("SELECT * FROM Win32_OperatingSystem");
+            ManagementObjectSearcher managementObjectSearcher = new(objectQuery);
             ManagementObjectCollection managementObjectCollection = managementObjectSearcher.Get();
 
             foreach (ManagementObject managementObject in managementObjectCollection)
             {
                 Int64 totalRamMB = Convert.ToInt64(managementObject["TotalVisibleMemorySize"]) / 1024;
 
-                
+                Starter_MinRamSlider.Minimum = 0;
+                Starter_MaxRamSlider.Minimum = 0;
                 Starter_MinRamSlider.Maximum = totalRamMB;
                 Starter_MaxRamSlider.Maximum = totalRamMB;
             }
 
-            if (Starter_MinRamSlider.Minimum < 0)
+            if (Starter_MinRamText.Text == null)
             {
-                Starter_MinRamSlider.Minimum = 0;
+                Starter_MinRamText.Text = "0";
             }
-            if (Starter_MaxRamSlider.Minimum < 0)
+            if (Starter_MaxRamText.Text == null)
             {
-                Starter_MaxRamSlider.Minimum = 0;
+                Starter_MaxRamText.Text = "0";
             }
-            
-            RegistryKey rk = Registry.LocalMachine;
-            RegistryKey subKey = rk.OpenSubKey("SOFTWARE\\JavaSoft\\Java Runtime Environment");
 
-            string currentVerion = subKey.GetValue("CurrentVersion").ToString();
 
-            Starter_JavaComboBox.Items.Add(currentVerion);
+            // If the values are already there then just load them.
+            string ServerFile = "server.jar";
+            string ServerPath = "C:\\Users\\Fayez\\OneDrive\\Desktop\\asd";
 
-            //try
-            //{
-            //    ProcessStartInfo psi = new ProcessStartInfo();
-            //    psi.FileName = "java.exe";
-            //    psi.Arguments = " -version";
-            //    psi.RedirectStandardError = true;
-            //    psi.UseShellExecute = false;
 
-            //    Process pr = Process.Start(psi);
-            //    string strOutput = pr.StandardError.ReadLine().Split(' ')[2].Replace("\"", "");
+            var startInfo = new ProcessStartInfo("java", "-Xmx" + Starter_MinRamText.Text + "M -Xms1024M -jar " + ServerFile + " nogui")
+            {
+                WorkingDirectory = ServerPath
+            };
 
-            //    Starter_JavaComboBox.Items.Add(strOutput);
-            //}
-            //catch (Exception ex)
-            //{
-            //    Console.WriteLine("Exception is " + ex.Message);
-            //}
+            Console_ConsoleOutputText.Text += "java -Xmx" + Starter_MinRamText.Text + "M -Xms" + Starter_MinRamText.Text + "M -jar " + ServerFile + " nogui";
+            startInfo.RedirectStandardInput = startInfo.RedirectStandardOutput = true;
+            startInfo.UseShellExecute = false; // Necessary for Standard Stream Redirection
+            startInfo.CreateNoWindow = true; // You can do either this or open it with "javaw" instead of "java"
+
+            ServerProc = new Process
+            {
+                StartInfo = startInfo,
+                EnableRaisingEvents = true
+            };
+            ServerProc.OutputDataReceived += new DataReceivedEventHandler(ServerProc_ErrorDataReceived);
+            ServerProc.Exited += new EventHandler(ServerProc_Exited);
         }
 
         private void Starter_BrowseJarFileBtn_Click(object sender, RoutedEventArgs e)
         {
-            OpenFileDialog dialog = new OpenFileDialog
-
+            OpenFileDialog dialog = new()
             {
                 Title = "Select server file",
                 Filter = "Java .jar files |*.jar",
@@ -82,12 +85,6 @@ namespace MinecraftUI
             dialog.ShowDialog();
             Starter_JarFileText.Text = dialog.FileName;
         }
-
-        /// <summary>
-        /// Sets icon, labels and icons to appropriate server based on selection
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
         private void Starter_ServerTypeCombo_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             var selectedValue = ((ComboBoxItem)Starter_ServerTypeCombo.SelectedItem).Content.ToString();
@@ -97,9 +94,6 @@ namespace MinecraftUI
 
             if (selectedValue == "Spigot")
             {
-
-
-
                 src.BeginInit();
                 src.UriSource = new Uri("../../assets/spigot_logo.png", UriKind.RelativeOrAbsolute);
                 src.EndInit();
@@ -114,20 +108,82 @@ namespace MinecraftUI
             }
         }
 
-        void wc_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        private void ServerProc_ErrorDataReceived(object sender, DataReceivedEventArgs e)
         {
-            progressBar.Value = e.ProgressPercentage;
+            // You have to do this through the Dispatcher because this method is called by a different Thread
+            Dispatcher.Invoke(new Action(() =>
+            {
+                Console_ConsoleOutputText.Text += e.Data + "\r\n";
+                Console_ConsoleInputText.ScrollToEnd();
+            }));
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void ServerProc_Exited(object sender, EventArgs e)
         {
-            using WebClient wc = new();
+            // The order of these 2 lines is very important, reversing them will cause an exception
+            // and you wont be able to read from the stream when you start the Process again !
+            ServerProc.CancelOutputRead();
+            ServerProc.Close();
+        }
 
-            Starter_Download_Content_Label.Content = "Downloading...";
-            wc.DownloadProgressChanged += wc_DownloadProgressChanged;
-            wc.Headers.Add("User-Agent: Other");
-            wc.DownloadFile("https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar", "BuildTools.jar");
-            Starter_Download_Content_Label.Content = "Downloaded";
+        private void Console_ConsoleInputText_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key != Key.Enter) return;
+            ServerProc.StandardInput.WriteLine(Console_ConsoleInputText.Text);
+            Console_ConsoleInputText.Clear();
+        }
+
+        private void Window_Closing(object sender, CancelEventArgs e)
+        {
+            try
+            {
+                ServerProc.StandardInput.WriteLine("stop");
+                ServerProc.WaitForExit(10000);
+                if (!ServerProc.HasExited)
+                {
+                    Console_ConsoleOutputText.Text += "ERROR: The Server doesn't want to Stop !\r\n";
+                    e.Cancel = true;
+                }
+            }
+            catch { }
+        }
+
+        private void Console_StartBtn_Click(object sender, RoutedEventArgs e)
+        {
+            // This is my dirty way of making sure that I don't start the Server Twice :D
+            // If the server isn't running then an exception will be thrown when accessing
+            // ServerProc.StartTime and the method wont return ;-)
+            try { var x = ServerProc.StartTime; return; }
+            catch { }
+
+            Console_ConsoleOutputText.Text = "";
+            ServerProc.Start();
+            ServerProc.BeginOutputReadLine();
+        }
+
+        private void Console_StopBtn_Click(object sender, RoutedEventArgs e)
+        {
+            try { ServerProc.StandardInput.WriteLine("stop");}
+            catch { }
+        }
+
+        private void Console_RestartBtn_Click(object sender, RoutedEventArgs e)
+        {
+            try {
+                ServerProc.StandardInput.WriteLine("stop");
+                var x = ServerProc.StartTime; return;
+            }
+            catch { }
+
+            Console_ConsoleOutputText.Text = "";
+            ServerProc.Start();
+            ServerProc.BeginOutputReadLine();
+
+        }
+        private new void PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
         }
     }
 }
